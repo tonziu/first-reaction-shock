8f04391d3ad1e7bcc4fc1b105761541b94789503
Tony De Corso <tony.dcorso@gmail.com>
2025-09-09 18:32:01 +0200

    CreateUserView

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_doatenv
+from dotenv import load_dotenv
 import os
 
-load_doatenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
